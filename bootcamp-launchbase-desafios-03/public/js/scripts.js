2d1c6aaef85a3b5a791cfbb8e32962a9e27719c1
vncsgonca23 <vncsgonca23@gmail.com>
2019-11-23 08:52:06 -0300

    DeleteChallengeModule03

--- bootcamp-launchbase-desafios-03/public/js/scripts.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll('.card');
-for (const card of cards) {
-  card.addEventListener('click', () => {
-    const cardId = card.getAttribute('id');
-    window.location.href = `/recipe?id=${cardId}`;
-  });
-}
